305f924b7176e1c7ae4e51fea1f23fb1c4136e3d
Nogo4 <noah.gomes@epitech.eu>
2026-03-18 19:21:06 +0100

    refactor(IGameModule): change GraphicsAPI class by IGameModule interface

--- IGameModule.hpp
+++ IGameModule.hpp
@@ -2,12 +2,12 @@
 
 #include <string>
 
-class GraphicsApi;
+class IDisplayModule;
 
 class IGameModule {
-public:
-    virtual ~IGameModule() = default;
+    public:
+        virtual ~IGameModule() = default;
 
-    virtual std::string &getName() = 0;
-    virtual void update(GraphicsApi *api) = 0;
+        virtual std::string& getName() = 0;
+        virtual void update(IDisplayModule* api) = 0;
 };
